069649981d814a312a245959caaffd65e77d94b5
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-07-05 10:59:18 -0300

    Se agrego la clase serie

---
+++ js/serieClass.js
@@ -0,0 +1,11 @@
+export class Serie {
+    constructor(codigo, titulo, descripcion, imagen, genero){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.genero = genero;
+    }
+    // agregar los metodos necesarios
+
+}
